3c6f8feb7b23f1559306050a9e6b6f323d3ebaa6
Siv-tspab <sbapst.j@gmail.com>
2021-11-02 11:07:51 +0100

    ADD: style to the nav

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
 
 export default function Nav() {
     return (
-        <header>
+        <header className="flex bg-black text-white">
+            <h1 className="nav-link font-semibold">RobotDemoApp</h1>
+            <ul className="flex">
+                <li className="nav-link">
+                    <Link to="/">
+                        Accueil
+                    </Link>
+                </li>
+                <li className="nav-link">
+                    <Link to="/articles">
+                        Liste des articles
+                    </Link>
+                </li>
+            </ul>
         </header>
     )
 }
